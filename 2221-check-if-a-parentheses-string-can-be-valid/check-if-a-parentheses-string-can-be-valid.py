544a9b564b2f5aa246fa577bc5a5176e8788c876
AaQib Ali <75785706+aaqibali1@users.noreply.github.com>
2025-01-12 23:21:38 +0500

    Time: 107 ms (52.74%) | Memory: 18.9 MB (25.07%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,27 +1,17 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        n=len(s)
-        if n&1==1: return False
+        if len(s) % 2 != 0:
+            return False
         
-        bMin, bMax=0, 0  # Initialize balance
+        def check(s, locked, left, right):
+            balance = 0
+            for i in range(len(s)):
+                if locked[i] == '0' or s[i] == left:
+                    balance += 1
+                else:
+                    balance -= 1
+                if balance < 0:
+                    return False
+            return True
         
-        for i, c in enumerate(s):
-            op=c== '('
-            wild=locked[i]=='0'
-            
-            # Update balances
-            if (not op) or wild: bMin-=1  
-            else: bMin+=1  
-            
-            if op or wild: bMax+=1  
-            else: bMax-=1  
-            
-            if bMax < 0: return False
-            
-            bMin = max(bMin, 0)
-        
-        # Check if the parentheses can be 
-        return bMin==0
-
-        
-        
+        return check(s, locked, '(', ')') and check(s[::-1], locked[::-1], ')', '(')
