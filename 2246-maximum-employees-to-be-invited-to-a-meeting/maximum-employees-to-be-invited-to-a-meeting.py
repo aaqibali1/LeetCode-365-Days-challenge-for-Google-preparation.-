6e3e25abc1eb58727878a6a043018a5bb1410f0f
AaQib Ali <75785706+aaqibali1@users.noreply.github.com>
2025-01-26 23:23:14 +0500

    Time: 161 ms (92.31%) | Memory: 31.3 MB (72.31%) - LeetSync

--- 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/maximum-employees-to-be-invited-to-a-meeting.py
@@ -1,58 +1,47 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def maximumInvitations(self, favorite: List[int]) -> int:
-        adj = defaultdict(list)
-        transpose = defaultdict(list)
+        n = len(favorite)
+        in_degree = [0] * n
         
-        for i in range(len(favorite)):
-            adj[i].append(favorite[i])
-            transpose[favorite[i]].append(i)
+        # Calculate in-degree for each node
+        for person in range(n):
+            in_degree[favorite[person]] += 1
         
-        stack = []
-        visited = [False] * len(favorite)
+        # Topological sorting to remove non-cycle nodes
+        q = deque()
+        for person in range(n):
+            if in_degree[person] == 0:
+                q.append(person)
         
-        def dfs(node):
-            visited[node] = True
-            for n in adj[node]:
-                if not visited[n]:
-                    dfs(n)
-            stack.append(node)
+        depth = [1] * n  # Depth of each node
         
-        for i in range(len(favorite)):
-            if not visited[i]:
-                dfs(i)
+        while q:
+            current_node = q.popleft()
+            next_node = favorite[current_node]
+            depth[next_node] = max(depth[next_node], depth[current_node] + 1)
+            in_degree[next_node] -= 1
+            if in_degree[next_node] == 0:
+                q.append(next_node)
         
-        sccs = []  # list of sets of each SCC
-        scc = set()
-        visited = [False] * len(favorite)
+        longest_cycle = 0
+        two_cycle_invitations = 0
         
-        def traverseForScc(node):
-            visited[node] = True
-            scc.add(node)
-            for n in transpose[node]:
-                if not visited[n]:
-                    traverseForScc(n)
+        # Detect cycles
+        for person in range(n):
+            if in_degree[person] == 0:  # Already processed
+                continue
+            
+            cycle_length = 0
+            current = person
+            while in_degree[current] != 0:
+                in_degree[current] = 0  # Mark as visited
+                cycle_length += 1
+                current = favorite[current]
+            
+            if cycle_length == 2:
+                # For 2-cycles, add the depth of both nodes
+                two_cycle_invitations += depth[person] + depth[favorite[person]]
+            else:
+                longest_cycle = max(longest_cycle, cycle_length)
         
-        while stack:
-            top = stack.pop()
-            if not visited[top]:
-                scc = set()
-                traverseForScc(top)
-                sccs.append(scc)
-        
-        ans = max([len(scc) if len(scc) != 2 else -1 for scc in sccs])
-
-        def findLongestArm(a, b):
-            l = 0
-            for node in transpose[a]:
-                if node != b:
-                    l = max(l, 1 + findLongestArm(node, b))
-            return l
-        
-        twoNodeSccs = 0
-        for n1, n2 in [scc for scc in sccs if len(scc) == 2]:
-            twoNodeSccs += 2 + findLongestArm(n1, n2) + findLongestArm(n2, n1)
-        
-        return max(ans, twoNodeSccs)
+        return max(longest_cycle, two_cycle_invitations)
